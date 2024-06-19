100301f049f8c68908297f7c1e80d318c8a61238
shuen.lyu <shuen.lyu@uct.com>
2024-06-19 10:57:45 +0800

    enable license for surveyJS library

--- checklist-frontend/src/index.tsx
+++ checklist-frontend/src/index.tsx
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { slk } from "survey-core";
+
+slk(
+     "ZGJkODBmZGItOTEzNi00OWY0LTgzZWQtZmQxMmJiNTQ1MDdmOzE9MjAyNS0wNi0wMywyPTIwMjUtMDYtMDMsND0yMDI1LTA2LTAz"
+);
+
+
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"></link>;
 
 if (process.env.NODE_ENV === "development") {
