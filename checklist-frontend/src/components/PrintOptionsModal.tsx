9cd2eb0328456dcef0210b70720ec61c053af57b
shuen.lyu <shuen.lyu@uct.com>
2024-07-01 15:30:30 +0800

    frontend: refactoring Run.tsx and remove fetchAllValues and getSurveys function, and export pdf onclick=handleSaveAndCloase

---
+++ checklist-frontend/src/components/PrintOptionsModal.tsx
@@ -0,0 +1,214 @@
+import React from "react";
+
+export interface PdfOptions {
+    fontSize: number;
+    fontName: string;
+    margins: {
+        left: number;
+        right: number;
+        top: number;
+        bot: number;
+    };
+    orientation: "p" | "l" | undefined;
+    format: string;
+}
+
+export interface PrintOptionsModalProps {
+    fileName: string;
+    setFileName: (value: string) => void;
+    pdfOptions: PdfOptions;
+    setPdfOptions: (options: PdfOptions) => void;
+    savePdf: () => void;
+    closeModal: () => void;
+}
+
+const PrintOptionsModal: React.FC<PrintOptionsModalProps> = ({
+    fileName,
+    setFileName,
+    pdfOptions,
+    setPdfOptions,
+    savePdf,
+    closeModal
+}) => {
+    const handleSaveAndClose = () => {
+        savePdf();
+        closeModal();
+    }
+    return (
+        <>
+            <div className="modal-container" onClick={closeModal}></div>
+            <div className="print-options-modal">
+                <div>
+                    <div className="flex">
+                        <label htmlFor="file-name" style={{ fontWeight: "600", display: "block" }}>
+                            File Name
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="Enter File Name"
+                            style={{ marginTop: 5, padding: "8px 4px", width: "99%" }}
+                            value={fileName}
+                            onChange={(e) => setFileName(e.target.value)}
+                        />
+                    </div>
+                    <div style={{ marginTop: 20 }}>
+                        <label htmlFor="paper-size" style={{ fontWeight: "600" }}>
+                            Paper Size
+                        </label>
+                        <select
+                            name="paper-size"
+                            className="select-dropdown"
+                            value={pdfOptions.format}
+                            onChange={(e) =>
+                                setPdfOptions({ ...pdfOptions, format: e.target.value })
+                            }
+                        >
+                            {/* Add other options as needed */}
+                            <option value="a4">A4</option>
+                            <option value="letter">Letter</option>
+                            <option value="legal">Legal</option>
+                        </select>
+                    </div>
+                    <div style={{ marginTop: 20 }}>
+                        <label htmlFor="orientation" style={{ fontWeight: "600" }}>
+                            Page Orientation
+                        </label>
+                        <select
+                            name="orientation"
+                            className="select-dropdown"
+                            value={pdfOptions.orientation}
+                            onChange={(e) =>
+                                setPdfOptions({
+                                    ...pdfOptions,
+                                    orientation: e.target.value as "p" | "l" | undefined,
+                                })
+                            }
+                        >
+                            <option value="p">Portrait</option>
+                            <option value="l">Landscape</option>
+                        </select>
+                    </div>
+                    <div style={{ marginTop: 20 }}>
+                        <label htmlFor="margin" style={{ fontWeight: "600" }}>
+                            Margins
+                        </label>
+                        <div style={{ display: "flex", justifyContent: "space-between", marginTop: 10 }}>
+                            <input
+                                type="number"
+                                placeholder="Left"
+                                className="w-20"
+                                value={pdfOptions.margins.left}
+                                onChange={(e) =>
+                                    setPdfOptions({
+                                        ...pdfOptions,
+                                        margins: {
+                                            ...pdfOptions.margins,
+                                            left: parseInt(e.target.value),
+                                        },
+                                    })
+                                }
+                            />
+                            <input
+                                type="number"
+                                placeholder="Top"
+                                className="w-20"
+                                value={pdfOptions.margins.top}
+                                onChange={(e) =>
+                                    setPdfOptions({
+                                        ...pdfOptions,
+                                        margins: {
+                                            ...pdfOptions.margins,
+                                            top: parseInt(e.target.value),
+                                        },
+                                    })
+                                }
+                            />
+                            <input
+                                type="number"
+                                placeholder="Right"
+                                className="w-20"
+                                value={pdfOptions.margins.right}
+                                onChange={(e) =>
+                                    setPdfOptions({
+                                        ...pdfOptions,
+                                        margins: {
+                                            ...pdfOptions.margins,
+                                            right: parseInt(e.target.value),
+                                        },
+                                    })
+                                }
+                            />
+                            <input
+                                type="number"
+                                placeholder="Bottom"
+                                className="w-20"
+                                value={pdfOptions.margins.bot}
+                                onChange={(e) =>
+                                    setPdfOptions({
+                                        ...pdfOptions,
+                                        margins: {
+                                            ...pdfOptions.margins,
+                                            bot: parseInt(e.target.value),
+                                        },
+                                    })
+                                }
+                            />
+                        </div>
+                    </div>
+                    <div style={{ marginTop: 20, display: "flex", justifyContent: "space-between" }}>
+                        <div style={{ width: "20%" }}>
+                            <label htmlFor="font-size" style={{ fontWeight: "600", display: "block" }}>
+                                Font Size
+                            </label>
+                            <input
+                                type="number"
+                                placeholder="Font Size"
+                                style={{ marginTop: 10, padding: "8px 4px", width: "100%" }}
+                                value={pdfOptions.fontSize}
+                                onChange={(e) =>
+                                    setPdfOptions({
+                                        ...pdfOptions,
+                                        fontSize: parseInt(e.target.value),
+                                    })
+                                }
+                            />
+                        </div>
+                        <div style={{ width: "70%" }}>
+                            <label htmlFor="font-style" style={{ fontWeight: "600" }}>
+                                Font Style
+                            </label>
+                            <select
+                                name="font-style"
+                                className="select-dropdown"
+                                style={{ marginTop: 10 }}
+                                value={pdfOptions.fontName}
+                                onChange={(e) =>
+                                    setPdfOptions({
+                                        ...pdfOptions,
+                                        fontName: e.target.value,
+                                    })
+                                }
+                            >
+                                <option value="Helvetica">Helvetica</option>
+                                <option value="Courier">Courier</option>
+                                <option value="Times">Times</option>
+                                <option value="Symbol">Symbol</option>
+                                <option value="ZapfDingbats">ZapfDingbats</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div className="buttons">
+                    <button className="modal-button cancel-button" onClick={closeModal}>
+                        Cancel
+                    </button>
+                    <button className="modal-button" onClick={handleSaveAndClose}>
+                        Export
+                    </button>
+                </div>
+            </div >
+        </>
+    );
+};
+
+export default PrintOptionsModal;
--- checklist-frontend/src/pages/Run.tsx
+++ checklist-frontend/src/pages/Run.tsx
@@ -1,71 +1,25 @@
 // require('dotenv').config();
 import { useParams } from "react-router";
-import {
-  ITheme,
-  Model,
-  StylesManager,
-  matrixDropdownColumnTypes,
-} from "survey-core";
+import { ITheme, Model, StylesManager, matrixDropdownColumnTypes } from "survey-core";
 
 import { Survey } from "survey-react-ui";
 import { SurveyPDF, SurveyHelper } from "survey-pdf";
 import "survey-core/defaultV2.css";
 import { useEffect, useRef, useState } from "react";
 import { useApi } from "../utils/api";
-import {
-  ContrastDark,
-  DefaultLight,
-  ContrastLight,
-  DefaultDark,
-  BorderlessDarkPanelless,
-  BorderlessDark,
-  BorderlessLightPanelless,
-  BorderlessLight,
-  ContrastDarkPanelless,
-  ContrastLightPanelless,
-  DefaultLightPanelless,
-  DoubleBorderDarkPanelless,
-  DoubleBorderDark,
-  DoubleBorderLightPanelless,
-  DoubleBorderLight,
-  FlatDarkPanelless,
-  FlatDark,
-  FlatLightPanelless,
-  FlatLight,
-  LayeredDarkPanelless,
-  LayeredDark,
-  LayeredLightPanelless,
-  LayeredLight,
-  PlainDarkPanelless,
-  PlainDark,
-  PlainLightPanelless,
-  PlainLight,
-  SharpDarkPanelless,
-  SharpDark,
-  SharpLightPanelless,
-  SharpLight,
-  SolidDarkPanelless,
-  SolidDark,
-  SolidLightPanelless,
-  SolidLight,
-  ThreeDimensionalDarkPanelless,
-  ThreeDimensionalDark,
-  ThreeDimensionalLightPanelless,
-  ThreeDimensionalLight
+import { themes } from "../utils/themeOptions";
+import PrintOptionsModal, { PrintOptionsModalProps, PdfOptions } from "../components/PrintOptionsModal";
 
-} from "survey-core/themes";
 import Logger from "../utils/logger";
 
 import { SurveyQuestionEditorDefinition } from "survey-creator-core";
 //survey helper functions
 SurveyHelper.GAP_BETWEEN_COLUMNS = 1;
 SurveyHelper.MATRIX_COLUMN_WIDTH = 3;
-
 // for showing signature pad on matrix drop down
 matrixDropdownColumnTypes.signaturepad = {};
 SurveyQuestionEditorDefinition.definition["matrixdropdowncolumn@signaturepad"] =
   {};
-
 // for showing image type on matrix drop down
 matrixDropdownColumnTypes.image = {};
 SurveyQuestionEditorDefinition.definition["matrixdropdowncolumn@image"] = {};
@@ -74,19 +28,6 @@ StylesManager.applyTheme("defaultV2");
 
 interface QuestionData {
   [key: string]: string | undefined;
-}
-
-interface PdfOptions {
-  fontSize: number;
-  fontName: string;
-  margins: {
-    left: number;
-    right: number;
-    top: number;
-    bot: number;
-  };
-  orientation: "p" | "l" | undefined;
-  format: string;
 }
 
 Logger.info("Process.env: ", process.env);
@@ -119,140 +60,31 @@ const Run = () => {
   });
 
   let model = new Model(survey.json);
-  let themes = {
-    0: DefaultLight,
-    1: DefaultDark,
-    2: ContrastLight,
-    3: ContrastDark,
-    4: BorderlessDarkPanelless,
-    5: BorderlessDark,
-    6: BorderlessLightPanelless,
-    7: BorderlessLight,
-    8: ContrastDarkPanelless,
-    9: ContrastLightPanelless,
-    10: DefaultLightPanelless,
-    11: DoubleBorderDarkPanelless,
-    12: DoubleBorderDark,
-    13: DoubleBorderLightPanelless,
-    14: DoubleBorderLight,
-    15: FlatDarkPanelless,
-    16: FlatDark,
-    17: FlatLightPanelless,
-    18: FlatLight,
-    19: LayeredDarkPanelless,
-    20: LayeredDark,
-    21: LayeredLightPanelless,
-    22: LayeredLight,
-    23: PlainDarkPanelless,
-    24: PlainDark,
-    25: PlainLightPanelless,
-    26: PlainLight,
-    27: SharpDarkPanelless,
-    28: SharpDark,
-    29: SharpLightPanelless,
-    30: SharpLight,
-    31: SolidDarkPanelless,
-    32: SolidDark,
-    33: SolidLightPanelless,
-    34: SolidLight,
-    35: ThreeDimensionalDarkPanelless,
-    36: ThreeDimensionalDark,
-    37: ThreeDimensionalLightPanelless,
-    38: ThreeDimensionalLight
-  };
 
+  //model applyTheme
   const storedTheme: string | null = localStorage.getItem("theme");
   let theme: ITheme;
-
   if (storedTheme !== null && themes.hasOwnProperty(storedTheme)) {
     const themeIndex: number = parseInt(storedTheme, 10);
     theme = themes[themeIndex as keyof typeof themes];
   } else {
     theme = themes[0];
   }
-
   model.applyTheme(theme);
 
-
-  const getSurveyResults = async () => {
-    const resultsAction = await fetchData("/results?postId=" + id);
-    Logger.debug('getSurveyResults: id from useParams, ', id);
-
-    let parsedQuestion = null;
-    for (const result of resultsAction.data) {
-      Logger.debug("getSurveyResults: Results are attampting to parse, ", result);
-      let parsed;
-      if (typeof result === 'string') {
-        try {
-          parsed = JSON.parse(result);
-        } catch (e) {
-          console.error("Error parsing JSON:", e);
-        }
-      } else {
-        // If it's already an object, no need to parse
-        parsed = result;
-      }
-      if (parsed.wo === term) {
-        parsedQuestion = parsed;
-        break;
-      }
-    }
-    Logger.debug("parsedQuestions from getSurveyResults: ", parsedQuestion);
-    return parsedQuestion;
-  };
+  Logger.debug("Run: theme applied, ", theme);
 
   const getSurvey = async () => {
     const response = await fetchData("/getSurvey?surveyId=" + id, false);
     setSurvey(response.data);
   };
 
-  const fetchValues = async () => {
-    if (term && survey.json.length > 0) {
-      let isGotFilled = false;
-      let data: QuestionData = {};
-      let getResults = new Promise(function (myResolve, myReject) {
-        const results = getSurveyResults();
-        results.then((res) => {
-          if (res) {
-            isGotFilled = true;
-            data = res;
-          }
-          myResolve("OK");
-        });
-      });
-      await getResults.then((response) => {
-        if (response === "OK" && isGotFilled) {
-          for (const page of JSON.parse(survey.json).pages) {
-            for (const elem of page.elements) {
-              let question = model.getQuestionByName(elem.name);
-              let name = elem.name;
-              question.value = data[name];
-            }
-          }
-        } else {
-          const subscribedQuestion = model.getQuestionByName("wo");
-          subscribedQuestion.value = term;
-          const omsQuestion = model.getQuestionByName("oms");
-          omsQuestion.value = oms_value;
-          const stepQuestion = model.getQuestionByName("step");
-          stepQuestion.value = step_value;
-          const stationQuestion = model.getQuestionByName("station");
-          stationQuestion.value = station_value;
-          const useridQuestion = model.getQuestionByName("userid");
-          useridQuestion.value = userid_value;
-        }
-      });
-    }
-  };
-  useEffect(() => {
-    fetchValues();
-  }, [survey.json]);
   useEffect(() => {
     getSurvey();
   }, []);
 
   function createSurveyPdfModel(surveyModel: any) {
-    console.log(surveyModel);
+    Logger.info(surveyModel);
 
     const surveyPDF = new SurveyPDF(survey.json, { ...pdfOptions });
     //Add this line
@@ -281,6 +113,17 @@ const Run = () => {
     model = modelRef.current;
     setPrintOptions(false);
   };
+
+  const subscribedQuestion = model.getQuestionByName("wo");
+  if (subscribedQuestion && term) subscribedQuestion.value = term;
+  const omsQuestion = model.getQuestionByName("oms");
+  if (omsQuestion && oms_value) omsQuestion.value = oms_value;
+  const stepQuestion = model.getQuestionByName("step");
+  if (omsQuestion && oms_value) stepQuestion.value = step_value;
+  const stationQuestion = model.getQuestionByName("station");
+  if (stationQuestion && station_value) stationQuestion.value = station_value;
+  const useridQuestion = model.getQuestionByName("userid");
+  if (useridQuestion && userid_value) useridQuestion.value = userid_value;
 
   model.onComplete.add(async (sender: Model) => {
     await postData("/post", {
@@ -326,232 +169,14 @@ const Run = () => {
 
       <Survey model={model} />
       {printOptions && (
-        <>
-          <div
-            className="modal-container "
-            onClick={() => closePrintModal()}
-          ></div>
-          <div className="print-options-modal">
-            <div>
-              <div>
-                <div>
-                  <div className="flex">
-                    <label
-                      htmlFor="file-name"
-                      style={{ fontWeight: "600", display: "block" }}
-                    >
-                      File Name
-                    </label>
-                    <input
-                      type="text"
-                      placeholder="Enter File Name"
-                      style={{
-                        marginTop: 5,
-                        padding: "8px 4px",
-                        width: "99%",
-                      }}
-                      value={fileName}
-                      onChange={(e) => setFileName(e.target.value)}
-                    />
-                  </div>
-                  <div style={{ marginTop: 20 }}>
-                    <label htmlFor="paper-size" style={{ fontWeight: "600" }}>
-                      Paper Size
-                    </label>
-                    <select
-                      name="paper-size"
-                      className="select-dropdown"
-                      value={pdfOptions.format}
-                      onChange={(e) =>
-                        setPdfOptions({ ...pdfOptions, format: e.target.value })
-                      }
-                    >
-                      <option value="a4">A4</option>
-                      <option value="a0">A0</option>
-                      <option value="a2">A2</option>
-                      <option value="a3">A3</option>
-                      <option value="b0">B0</option>
-                      <option value="c0">C0</option>
-                      <option value="dl">DL</option>
-                      <option value="letter">Letter</option>
-                      <option value="government-letter">
-                        Government Letter
-                      </option>
-                      <option value="legal">Legal</option>
-                      <option value="junior-legal">Junior Legal</option>
-                      <option value="ledger">Ledger</option>
-                      <option value="tabloid">Tabloid</option>
-                      <option value="credit-card">Credit Card</option>
-                    </select>
-                  </div>
-                  <div style={{ marginTop: 20 }}>
-                    <label htmlFor="orientation" style={{ fontWeight: "600" }}>
-                      Page Orientation
-                    </label>
-                    <select
-                      name="orientation"
-                      className="select-dropdown"
-                      value={pdfOptions.orientation}
-                      onChange={(e) =>
-                        setPdfOptions({
-                          ...pdfOptions,
-                          orientation: e.target.value as "p" | "l" | undefined,
-                        })
-                      }
-                    >
-                      <option value="p">Portrait</option>
-                      <option value="l">Landscape</option>
-                    </select>
-                  </div>
-                  <div
-                    style={{
-                      marginTop: 20,
-                    }}
-                  >
-                    <label htmlFor="margin" style={{ fontWeight: "600" }}>
-                      Margins
-                    </label>
-                    <div
-                      style={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                        marginTop: 10,
-                      }}
-                    >
-                      <input
-                        type="number"
-                        placeholder="Left"
-                        className="w-20"
-                        value={pdfOptions.margins.left}
-                        onChange={(e) =>
-                          setPdfOptions({
-                            ...pdfOptions,
-                            margins: {
-                              ...pdfOptions.margins,
-                              left: parseInt(e.target.value),
-                            },
-                          })
-                        }
-                      />
-                      <input
-                        type="number"
-                        placeholder="Top"
-                        className="w-20"
-                        value={pdfOptions.margins.top}
-                        onChange={(e) =>
-                          setPdfOptions({
-                            ...pdfOptions,
-                            margins: {
-                              ...pdfOptions.margins,
-                              top: parseInt(e.target.value),
-                            },
-                          })
-                        }
-                      />
-                      <input
-                        type="number"
-                        placeholder="Right"
-                        className="w-20"
-                        value={pdfOptions.margins.right}
-                        onChange={(e) =>
-                          setPdfOptions({
-                            ...pdfOptions,
-                            margins: {
-                              ...pdfOptions.margins,
-                              right: parseInt(e.target.value),
-                            },
-                          })
-                        }
-                      />
-                      <input
-                        type="number"
-                        placeholder="Bottom"
-                        className="w-20"
-                        value={pdfOptions.margins.bot}
-                        onChange={(e) =>
-                          setPdfOptions({
-                            ...pdfOptions,
-                            margins: {
-                              ...pdfOptions.margins,
-                              bot: parseInt(e.target.value),
-                            },
-                          })
-                        }
-                      />
-                    </div>
-                  </div>
-                  <div
-                    style={{
-                      marginTop: 20,
-                      display: "flex",
-                      justifyContent: "space-between",
-                    }}
-                  >
-                    <div style={{ width: "20%" }}>
-                      <label
-                        htmlFor="font-size"
-                        style={{ fontWeight: "600", display: "block" }}
-                      >
-                        Font Size
-                      </label>
-                      <input
-                        type="number"
-                        placeholder="Bottom"
-                        style={{
-                          marginTop: 10,
-                          padding: "8px 4px",
-                          width: "100%",
-                        }}
-                        value={pdfOptions.fontSize}
-                        onChange={(e) =>
-                          setPdfOptions({
-                            ...pdfOptions,
-                            fontSize: parseInt(e.target.value),
-                          })
-                        }
-                      />
-                    </div>
-                    <div style={{ width: "70%" }}>
-                      <label htmlFor="font-size" style={{ fontWeight: "600" }}>
-                        Font Style
-                      </label>
-                      <select
-                        name="font-size"
-                        className="select-dropdown"
-                        style={{ marginTop: 10 }}
-                        value={pdfOptions.fontName}
-                        onChange={(e) =>
-                          setPdfOptions({
-                            ...pdfOptions,
-                            fontName: e.target.value,
-                          })
-                        }
-                      >
-                        <option value="Helvetica">Helvetica</option>
-                        <option value="Courier">Courier</option>
-                        <option value="Times">Times</option>
-                        <option value="Symbol">Symbol</option>
-                        <option value="ZapfDingbats">ZapfDingbats</option>
-                      </select>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="buttons">
-                <button
-                  className="modal-button cancle-button"
-                  onClick={() => closePrintModal()}
-                >
-                  Cancel
-                </button>
-                <button className="modal-button" onClick={() => savePdf()}>
-                  Export
-                </button>
-              </div>
-            </div>
-          </div>
-        </>
+        <PrintOptionsModal
+          fileName={fileName}
+          setFileName={setFileName}
+          pdfOptions={pdfOptions}
+          setPdfOptions={setPdfOptions}
+          savePdf={savePdf}
+          closeModal={closePrintModal}
+        />
       )}
     </>
   );
