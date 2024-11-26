e476eaa8fe56af23ccd42b5c960d32d78eaebe20
shuen <shuen.lyu@uct.com>
2024-11-26 00:30:41 -0500

    4.make add checklist, copy, edit and remove button work properly

--- checklist-frontend/src/components/EditModal.tsx
+++ checklist-frontend/src/components/EditModal.tsx
@@ -3,20 +3,28 @@ import { customers } from '../models/customer';
 import { products } from '../models/product';
 import SpinnerComponent from './SpinnerComponent';
 
+interface Folder {
+  id: number;
+  name: string;
+}
+
 interface EditModalProps {
   isOpen: boolean;
   onClose: () => void;
-  onSubmit: (data:{
+  onSubmit: (data: {
     name: string;
     customerId: number;
     productId: number;
+    folderId: number;
   }) => void;
-  initialData?:{
-    name:string;
+  initialData?: {
+    name: string;
     customerId: number;
     productId: number;
+    folderId: number;
   };
   title: string;
+  folders: Folder[];
 }
 
 
@@ -26,39 +34,44 @@ const EditModal: React.FC<EditModalProps> = ({
   onSubmit,
   initialData,
   title,
+  folders,
 }) => {
   const [inputValue, setInputValue] = useState('');
   const [customer, setCustomer] = useState<number>(0);
   const [product, setProduct] = useState<number>(0);
-  const [isValidChecklist, setIsValidChecklist] = useState(true); 
+  const [folder, setFolder] = useState<number>(0);
+  const [isValidChecklist, setIsValidChecklist] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(()=>{
-    if(initialData){
+  useEffect(() => {
+    if (initialData) {
       setInputValue(initialData.name);
       setCustomer(initialData.customerId);
       setProduct(initialData.productId);
+      setFolder(initialData.folderId);
     } else {
       setInputValue('');
       setCustomer(0);
       setProduct(0);
+      setFolder(0);
     }
-  },[initialData, isOpen]);
+  }, [initialData, isOpen]);
 
 
   if (!isOpen) return null;
 
   const handleSubmit = async () => {
-    if(inputValue === '' || customer === 0 || product === 0){
+    if (inputValue === '' || customer === 0 || product === 0 || folder === 0) {
       setIsValidChecklist(false);
       return;
     }
     setIsLoading(true);
-    try{
+    try {
       await onSubmit({
         name: inputValue,
         customerId: customer,
         productId: product,
+        folderId: folder,
       });
       onClose();
     } finally {
@@ -135,6 +148,30 @@ const EditModal: React.FC<EditModalProps> = ({
               </select>
             </div>
             <div style={{ marginTop: 20 }}>
+              <label htmlFor='folder' style={{ fontWeight: 600 }}>
+                Select Folder
+              </label>
+              <select
+                name='folder'
+                className='select-dropdown'
+                value={folder}
+                onChange={(e) => {
+                  setFolder(parseInt(e.target.value) || 0);
+                  setIsValidChecklist(true);
+                }}>
+                <option value=''>Choose a folder from list</option>
+                {folders.map((folder) => (
+                  <option
+                    key={folder.id}
+                    style={{ textTransform: 'capitalize' }}
+                    value={folder.id}
+                  >
+                    {folder.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div style={{ marginTop: 20 }}>
               <label htmlFor="name" style={{ fontWeight: 600 }}>
                 Checklist Name
               </label>
@@ -158,7 +195,7 @@ const EditModal: React.FC<EditModalProps> = ({
           <div className="buttons" style={{
             display: 'flex',
             justifyContent: 'flex-end',
-            gap:'10px',
+            gap: '10px',
             marginTop: '20px',
           }}>
             <button className="modal-button cancel-button" onClick={onClose}>
@@ -169,7 +206,7 @@ const EditModal: React.FC<EditModalProps> = ({
               onClick={handleSubmit}
               disabled={isLoading}
             >
-              {isLoading ? <SpinnerComponent/> : title === 'Enter New Checklist' ? 'Add' : 'Update'}
+              {isLoading ? <SpinnerComponent /> : title === 'Enter New Checklist' ? 'Add' : 'Update'}
             </button>
           </div>
         </div>
