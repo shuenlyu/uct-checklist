dd877e0bf6f7c79cc5b0a4c568f93a9cb49e5037
shuen <shuen.lyu@uct.com>
2025-04-09 07:05:00 -0400

    remove backend UI

--- checklist-backend/index.js
+++ checklist-backend/index.js
@@ -664,7 +664,10 @@ app.post("/folders", async (req, res) => {
   }
 });
 
-app.use(express.static(__dirname + "/public"));
+// app.use(express.static(__dirname + "/public"));
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Not Found" });
+});
 app.listen(process.env.PORT || 3002, () => {
   console.log("Server is listening on port", process.env.PORT || 3002);
 });
