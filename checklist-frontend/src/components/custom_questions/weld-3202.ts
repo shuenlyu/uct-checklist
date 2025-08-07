cf976db0550feadc126badb372d8269923ac6a00
Parthib Rathinam <Parthib.R@uct.com>
2025-08-07 08:52:06 +0800

    updated weldment custom component

---
+++ checklist-frontend/src/components/custom_questions/weld-3202.ts
@@ -0,0 +1,102 @@
+export const weld3202_json = {
+  name: "weldment_header",
+  defaultQuestionTitle: "Weldment Header",
+  hideNumber: true,
+  titleLocation: "hidden",
+  elementsJSON: [
+    {
+      type: "panel",
+      name: "panel3",
+      elements: [
+        {
+          type: "text",
+          name: "uctPartNum",
+          title: "UCT Part Number",
+        },
+        {
+          type: "text",
+          name: "rev",
+          startWithNewLine: false,
+          title: "Rev",
+        },
+        {
+          type: "text",
+          name: "qty",
+          startWithNewLine: false,
+          title: "Qty",
+        },
+      ],
+    },
+    {
+      type: "panel",
+      name: "panel6",
+      startWithNewLine: false,
+      elements: [
+        {
+          type: "text",
+          name: "date",
+          startWithNewLine: false,
+          title: "Date",
+          maskType: "datetime",
+          maskSettings: {
+            pattern: "mm-dd-yyyy",
+          },
+        },
+        {
+          type: "radiogroup",
+          name: "shipTo",
+          title: "Shipt To",
+          choices: [
+            {
+              value: "assembly",
+              text: "To Assembly",
+            },
+            {
+              value: "transfer",
+              text: "To Intersite Transfer",
+            },
+            {
+              value: "spareParts",
+              text: "Ship along/Spare parts",
+            },
+            {
+              value: "fastTrack",
+              text: "Fast Track",
+            },
+            {
+              value: "hardCap",
+              text: "Hard Cap/Plug Required",
+            },
+          ],
+        },
+        {
+          type: "boolean",
+          name: "fairRequires",
+          title: "If requires FAIR",
+        },
+      ],
+    },
+    {
+      type: "panel",
+      name: "panel5",
+      startWithNewLine: false,
+      elements: [
+        {
+          type: "text",
+          name: "woNum",
+          title: "WO Number",
+        },
+          {
+          type: "text",
+          name: "operation",
+          title: "Operation",
+        },
+        {
+          type: "comment",
+          name: "customer",
+          title: "Customer",
+        },
+      ],
+    },
+  ],
+};
